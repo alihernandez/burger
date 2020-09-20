3fde07d6a22f49f942b41ee0dcf0dbfd986e800b
phantomramen <ali.hernandez9two@gmail.com>
2020-09-20 13:16:45 -0600

    orm error

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -18,7 +19,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
